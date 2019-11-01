f114861ea40cccd7a49d9e0ca3e5c187f4dcb63a
whufclee <lee.randall@hoodgroup.co.uk>
2019-11-01 14:44:59 +0000

    Added duplicatechars

---
+++ duplicatechars/CodeWars - duplicate chars in string/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckDuplicatesInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool myReturn = Kata.IsIsogram("Leigh");
+            Console.WriteLine($"Result: {myReturn}");
+            Console.ReadLine();
+        }
+
+        public class Kata
+        {
+            public static bool IsIsogram(string str)
+            {
+                return str.Length == str.ToUpper().Distinct().Count();
+            }
+        }
+    }
+}
