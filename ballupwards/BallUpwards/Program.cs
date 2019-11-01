c1c31a208452c16b6b79b28c94c8bdadc5a20b1f
whufclee <lee.randall@hoodgroup.co.uk>
2019-11-01 14:38:24 +0000

    Added ballupwards

---
+++ ballupwards/BallUpwards/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallUpwards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ball.MaxBall(15);
+            Console.ReadLine();
+        }
+    }
+    public class Ball
+    {
+
+        public static int MaxBall(int v0)
+        {
+            double kmpm = (v0 * 1000) / 60;
+            double mps = kmpm / 60;
+            Console.WriteLine($"MPS: {mps}");
+            double orig_t = 0.166666666666667;
+            double g = Math.Pow(9.81, 2)*orig_t;
+            double t = 0.0;
+            for (int i = 0; i < v0; i++)
+            {
+                t += orig_t;
+                double h = (mps*t) - (0.5*g*t*t);
+                Console.WriteLine($"{i} : {h}");
+            }
+            return 8;
+        }
+    }
+}
---
+++ buildtower/BuildTower/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildTower
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] result = Kata.TowerBuilder(10);
+            foreach (string n in result) { Console.WriteLine(n); }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public class Kata
+        {
+            public static string[] TowerBuilder(int nFloors)
+            {
+                string[] tower = new string[nFloors];
+                int width = (nFloors * 2) - 1;
+                int spaces = width / 2;
+                for (int x = 0; x < nFloors; x++)
+                {
+                    string stars;
+                    string spacing = new string(' ', spaces - x);
+                    stars = new string('*', width - (spacing.Length * 2));
+                    tower[x] = spacing+stars+spacing;
+                }
+                return tower;
+            }
+        }
+    }
+}
